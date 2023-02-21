872b758e209777f905d58e1f9ca6817e336dd493
NoahLawu <noahlawu10@gmail.com>
2023-02-21 15:59:32 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ProfilePictureGenerator />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProfilePictureGenerator from "./components/ProfilePictureGenerator.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ProfilePictureGenerator,
+  },
+};
 </script>
 
 <style>
@@ -19,8 +18,14 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #e0ffff;
+}
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
 }
 </style>
---
+++ src/components/ProfilePictureGenerator.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="showImage" class="form">
+      <input
+        v-model="value"
+        class="first"
+        type="text"
+        placeholder="Ketik nama Anda..."
+      />
+      <button class="second" type="submit">Buat Profile Picture!</button>
+      <img class="img" alt="" :src="imgUrl" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfilePictureGenerator",
+  data() {
+    return {
+      value: "",
+      imgUrl: "",
+    };
+  },
+  methods: {
+    showImage() {
+      return (this.imgUrl = "https://robohash.org/" + this.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 20px;
+}
+
+.img {
+  display: block;
+  border-radius: 50%;
+  border: 1px solid grey;
+  background-color: white;
+}
+.first {
+  width: 250px;
+  height: 25px;
+  border-radius: 5px;
+  display: block;
+}
+.second {
+  width: 150px;
+  height: 40px;
+  background-color: seagreen;
+  color: white;
+  font: 14px/3 sans-serif;
+  border-radius: 5px;
+  padding-bottom: 45px;
+  cursor: pointer;
+  display: block;
+}
+
+input,
+input::placeholder {
+  font: 17px/3 sans-serif;
+}
+input {
+  padding: 0 10px;
+}
+</style>
